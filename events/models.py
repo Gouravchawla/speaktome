9e23dffa961c35fbc34527dbdfe95fe9fe319186
Gourav Chawla <gouravchawla@live.com>
2018-02-03 07:10:38 +0530

    Renamed polls to events and addd model

---
+++ events/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=100)
+    event_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
+
+
+class Question(models.Model):
+    event = models.ForeignField(Event, on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=255)
+    pub_date = models.DateTimeField('Published Date')
+
+    def __str__(self):
+        return self.question_text
+
+
+class Option(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    option_text = models.CharField(max_length=100)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.option_text + ' - ' + str(self.votes)
+
